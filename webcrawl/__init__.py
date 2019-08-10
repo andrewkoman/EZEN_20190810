5040f819790c77a82becc8d599fc7b38e7701afa
andrewkoman <ulderaman@gmail.com>
2019-08-10 11:48:25 +0900

    190810 1150

---
+++ webcrawl/__init__.py
@@ -0,0 +1,9 @@
+from webcrawl.bugsmusic import BugsCrawler
+
+if __name__ == '__main__' :
+    print('a. 국회 크롤링: ')
+    print('b. 벅스 크롤링: ')
+    #print('3. 연락처 삭제: ')
+    print('0. 종료 ')
+    #bm = BugsCrawler('https://music.bugs.co.kr/chart/track/realtime/total?chartdate=20190810&charthour=11')
+    #bm.scap()
